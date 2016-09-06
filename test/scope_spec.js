cab689fe2eef05a1d43d7006efbbe6ba54030347
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-09-07 08:48:56 +1000

    NaNs

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -201,6 +201,24 @@ describe('Scope', function() {
       scope.$digest();
       expect(scope.counter).toBe(2);
     });
+
+    it('correct handles NaNs', function() {
+      scope.number = 0/0; //NaN
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope) { return scope.number; },
+        function(newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+    });
   });
 
 });
