bfa8fe586601ecffc3984567920d62398e6ea145
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-09-24 14:08:07 +1000

    Broadcasting Scope Removal

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1970,6 +1970,24 @@ describe('Scope Inheritance', function(){
 
       expect(listener2).toHaveBeenCalled();
     });
+
+    it('fires $destroy when destroyed', function() {
+      var listener = jasmine.createSpy();
+      scope.$on('$destroy', listener);
+
+      scope.$destroy();
+
+      expect(listener).toHaveBeenCalled();
+    });
+
+    it('fires $destroy on children destroyed', function() {
+      var listener = jasmine.createSpy();
+      child.$on('$destroy', listener);
+
+      scope.$destroy();
+
+      expect(listener).toHaveBeenCalled();
+    });
   });
 });
 
