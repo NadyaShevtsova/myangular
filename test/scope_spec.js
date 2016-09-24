b1238a11249ebcfcafa202c7cf3388d1f1287798
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-09-24 14:11:56 +1000

    Disabling Listeners On Destroyed Scopes

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1988,6 +1988,16 @@ describe('Scope Inheritance', function(){
 
       expect(listener).toHaveBeenCalled();
     });
+
+    it('no longers calls listeners after destroyed', function() {
+      var listener = jasmine.createSpy();
+      scope.$on('myEvent', listener);
+
+      scope.$destroy();
+
+      scope.$emit('myEvent');
+      expect(listener).not.toHaveBeenCalled();
+    });
   });
 });
 
