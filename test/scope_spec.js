ecdc38dfab5f887daa189552a67ab6848639ee85
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-09-24 14:18:10 +1000

    Handling Exceptions

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1798,6 +1798,20 @@ describe('Scope Inheritance', function(){
 
         expect(event.defaultPrevented).toBe(true);
       });
+
+      it('does not stop on exceptions on ' + method, function() {
+        var listener1 = function(event) {
+          throw 'listener1 throwing an exception';
+        };
+        var listener2 = jasmine.createSpy();
+
+        scope.$on('someEvent', listener1);
+        scope.$on('someEvent', listener2);
+
+        scope[method]('someEvent');
+
+        expect(listener2).toHaveBeenCalled();
+      });
     });
 
     it('propagates up the scope hierarchy on $emit', function() {
@@ -1998,6 +2012,7 @@ describe('Scope Inheritance', function(){
       scope.$emit('myEvent');
       expect(listener).not.toHaveBeenCalled();
     });
+
   });
 });
 
