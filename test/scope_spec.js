718838a432fb2eeb7d8cea728f6c790e68418316
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-09-24 14:01:59 +1000

    Preventing Default Event Behavior

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1787,6 +1787,17 @@ describe('Scope Inheritance', function(){
 
         expect(nextListener).toHaveBeenCalled();
       });
+
+      it('is sets defaultPrevented when preventDefault called on ' + method, function() {
+        var listener = function(event) {
+          event.preventDefault();
+        };
+        scope.$on('someEvent', listener);
+
+        var event = scope[method]('someEvent');
+
+        expect(event.defaultPrevented).toBe(true);
+      });
     });
 
     it('propagates up the scope hierarchy on $emit', function() {
