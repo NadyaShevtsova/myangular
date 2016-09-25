9efce2515c86821f34368d1a0749a7e24ad52738
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-09-26 08:51:50 +1000

    CHAPTER 7
    Simple Attribute Lookup

--- src/parse.js
+++ src/parse.js
@@ -185,6 +185,8 @@ AST.prototype.primary = function() {
     return this.object();
   } else if (this.constants.hasOwnProperty(this.tokens[0].text)) {
     return this.constants[this.consume().text];
+  } else if (this.peek().identifier) {
+    return this.identifier();
   } else {
     return this.constant();
   }
@@ -267,10 +269,14 @@ function ASTCompiler(astBuilder) {
 
 ASTCompiler.prototype.compile = function(text) {
   var ast = this.astBuilder.ast(text);
-  this.state = {body: []};
+  this.state = {body: [], nextId: 0, vars: []};
   this.recurse(ast);
   /* jshint -W054*/
-  return new Function(this.state.body.join(''));
+  return new Function('s',
+    (this.state.vars.length ?
+      'var ' + this.state.vars.join(',') + ';' :
+      ''
+    ) + this.state.body.join(''));
   /* jshint -W054*/
 };
 
@@ -294,7 +300,29 @@ ASTCompiler.prototype.recurse = function(ast) {
       return key + ':' + value;
     }, this));
     return '{' + properties.join(',') + '}';
+  case AST.Identifier:
+    var introId = this.nextId();
+    this.if_('s', this.assign(introId, this.nonComputedMember('s', ast.name)));
+    return introId;
   }
+};
+
+ASTCompiler.prototype.nonComputedMember = function(left, right) {
+  return '(' + left + ').' + right;
+};
+
+ASTCompiler.prototype.if_ = function(test, consequent) {
+  this.state.body.push('if(', test, '){', consequent, '}');
+};
+
+ASTCompiler.prototype.assign = function(id, value) {
+  return id + '=' + value + ';';
+};
+
+ASTCompiler.prototype.nextId = function() {
+  var id = 'v' + (this.state.nextId++);
+  this.state.vars.push(id);
+  return id;
 };
 
 ASTCompiler.prototype.escape = function(value) {
