0ed783a2c579efe6a62e96d13dc48a729af5b899
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-10-02 18:40:35 +1000

    Function Calls

--- src/parse.js
+++ src/parse.js
@@ -25,7 +25,7 @@ Lexer.prototype.lex = function(text) {
       this.readNumber();
     } else if (this.is('\'"')) {
       this.readString(this.ch);
-    } else if (this.is('[],{}:.')) {
+    } else if (this.is('[],{}:.()')) {
       this.tokens.push({
         text: this.ch
       });
@@ -171,6 +171,7 @@ AST.Identifier = 'Identifier';
 AST.ThisExpression = 'ThisExpression';
 AST.LocalExpression = 'LocalExpression';
 AST.MemberExpression = 'MemberExpression';
+AST.CallExpression = 'CallExpression';
 
 AST.prototype.ast = function(text) {
   this.tokens = this.lexer.lex(text);
@@ -195,7 +196,7 @@ AST.prototype.primary = function() {
     primary = this.constant();
   }
   var next;
-  while ((next = this.expect('.', '['))) {
+  while ((next = this.expect('.', '[', '('))) {
     if (next.text === '[') {
       primary = {
         type: AST.MemberExpression,
@@ -204,16 +205,33 @@ AST.prototype.primary = function() {
         computed: true
       };
       this.consume(']');
-    } else {
+    } else if (next.text === '.') {
       primary = {
         type: AST.MemberExpression,
         object: primary,
         property: this.identifier(),
         computed: false
       };
+    } else if (next.text ==='(') {
+      primary = {
+        type: AST.CallExpression,
+        callee: primary,
+        arguments: this.parseArguments()
+      };
+      this.consume(')');
     }
   }
   return primary;
+};
+
+AST.prototype.parseArguments = function() {
+  var args = [];
+  if (!this.peek(')')) {
+    do {
+      args.push(this.primary());
+    } while (this.expect(','));
+  }
+  return args;
 };
 
 AST.prototype.object = function() {
@@ -352,6 +370,12 @@ ASTCompiler.prototype.recurse = function(ast) {
     return intoId;
   case AST.LocalsExpression:
     return 'l';
+  case AST.CallExpression:
+    var callee = this.recurse(ast.callee);
+    var args = _.map(ast.arguments, _.bind(function(arg) {
+      return this.recurse(arg);
+    }, this));
+    return callee + '&&' + callee + '(' + args.join(',') + ')';
   }
 };
 
